3cbf7e1ef8812ee290eec63574cf4effce4e0bac
KO-d14 <ainajk@hawaii.edu>
2023-09-20 08:04:10 +0900

    Initial Commit

---
+++ Hackwich4_9.19.23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackwich4_9.19.23
+//
+//  Created by Aina Kodaira on 9/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
