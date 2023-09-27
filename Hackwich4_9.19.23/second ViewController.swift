13e5c36d59b5684665ee736d5b89c9550d25c337
KO-d14 <ainajk@hawaii.edu>
2023-09-28 07:16:24 +0900

    Completed Parts 1-10 (in class)

---
+++ Hackwich4_9.19.23/second ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  second ViewController.swift
+//  Hackwich4_9.19.23
+//
+//  Created by Aina Kodaira on 9/20/23.
+//
+
+import UIKit
+
+class second_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
