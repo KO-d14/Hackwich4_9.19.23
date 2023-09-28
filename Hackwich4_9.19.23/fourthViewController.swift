14926a30cabe46b5fd414aefc083b33e2ebf1408
KO-d14 <ainajk@hawaii.edu>
2023-09-28 09:19:06 +0900

    Completed PART 11: On Your Own Problem Set & Part 12 - Screen Record Simulator (Uploaded on drive and sent link on laulima)

---
+++ Hackwich4_9.19.23/fourthViewController.swift
@@ -0,0 +1,40 @@
+//
+//  fourthViewController.swift
+//  Hackwich4_9.19.23
+//
+//  Created by Aina Kodaira on 9/28/23.
+//
+
+//NOTE: GUIDELINE DIDN'T SAY SPECIFICALLY TO CREATE BUT DID ANYWAY JUST IN CASE, ESPECIALLY BC WE'RE TO CREATE AN IBACTION
+
+import UIKit
+
+class fourthViewController: UIViewController {
+
+//Label will be on top & currently in color red 
+    @IBOutlet weak var myFavoriteFoodsLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func myFavoriteFoodsButtonPressed(_ sender: Any)
+    {
+        self.myFavoriteFoodsLabel.text = "Dim-Sum, Ice Cream, Bread"
+    }
+    
+}
---
+++ Hackwich4_9.19.23/thirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  thirdViewController.swift
+//  Hackwich4_9.19.23
+//
+//  Created by Aina Kodaira on 9/28/23.
+//
+
+
+// NOTE: GUIDELINE DIDN'T SAY SPECIFICALLY TO CREATE BUT DID ANYWAY JUST IN CASE
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
